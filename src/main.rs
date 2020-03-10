3ba2c2124c23c9058f5d872afda4429b77280514
Saim Irfan <samfan95@gmail.com>
2020-03-10 18:13:20 +0500

    completed task 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+
+//  Calculate Area of a Circle  
+
 fn main() {
-    println!("Hello, world!");
+    // Setting value of pie
+    const PI: i32 = 22/7;
+
+    // Taking input from user
+    let mut radius = String::new();
+    io::stdin().read_line(&mut radius)
+        .expect("Something went wrong");
+    let radius: i32 = match radius.trim().parse() {
+        Ok(num) => num,
+        Err(_) => panic!("Only enter numbers!!!"),
+    };
+
+    // Calculating Area
+    let area = PI * (radius*radius);
+    println!("Area of Circle with radius {} is {}", radius, area);
 }
+
